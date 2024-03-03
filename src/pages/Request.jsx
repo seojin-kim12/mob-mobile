dc347ed6053eab516b79d7f2c06c65395e84d687
seojin-kim12 <rlatjwls10403@naver.com>
2024-03-03 22:19:30 +0900

    의뢰서 수정

--- src/pages/Request.jsx
+++ src/pages/Request.jsx
@@ -239,7 +239,14 @@ const Request = () => {
   const navigate = useNavigate();
 
   const onClickCheck = () => {
-    navigate("/RequestCheck");
+    // 동의 체크 여부에 따라 다르게 동작하도록 설정
+    if (agree === "1") {
+      // 동의했을 경우 페이지 이동
+      navigate("/RequestCheck");
+    } else {
+      // 동의하지 않았을 경우 페이지 새로고침
+      window.location.reload();
+    }
   };
 
   const onClickBack = () => {
