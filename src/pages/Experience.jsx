ef26df5e69e37ca8ccb064098b82e5a99c535c17
seojin-kim12 <rlatjwls10403@naver.com>
2024-03-09 17:33:35 +0900

    bar위치 수정2

--- src/pages/Experience.jsx
+++ src/pages/Experience.jsx
@@ -74,7 +74,7 @@ const Container = styled.div`
     width: 0px;
     background-color: rgba(0, 0, 0, 0.9);
     margin-left: 590px;
-    margin-top: -820px;
+    margin-top: -750px;
 
     p {
       display: none;
